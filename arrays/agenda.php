640c6dc35d0d37c4b22de7fbeecdd531726e1858
jmenoscal <jmenoscal@gmail.com>
2024-03-11 14:13:49 +0100

    hola

--- arrays/agenda.php
+++ arrays/agenda.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Agenda con Array Multi dimensional</title>
+    <title>Agenda con Array Multi dimensional.</title>
 </head>
 
 <body>
