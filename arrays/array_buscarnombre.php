5b72da25ddff5f9cff3a8fb6f470c9097ca52996
jmenoscal <jmenoscal@gmail.com>
2024-03-11 14:27:48 +0100

    ok

--- arrays/array_buscarnombre.php
+++ arrays/array_buscarnombre.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>arrays asociativos</title>
+    <title>arrays asociativos.</title>
 </head>
 <body>
     <?php
